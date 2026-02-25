e57ceeb90595580e6e6e1840f374a264c37944e6
Momcilo Milic <momcilo.milic@gmail.com>
2026-02-25 13:27:22 +0100

    Initial commit

---
+++ build/faq-accordion-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'db1fc55eb66fe067bab5');
---
+++ build/faq-accordion-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '61d5c8ede491562e16cb');
